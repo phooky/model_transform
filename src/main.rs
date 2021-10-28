d4e78eea8d52c4f015286fd0c4abb33c375b6881
phooky <phooky@gmail.com>
2021-10-27 21:11:53 -0400

    gone about as far as I can with clap this go-round; it's pretty ugly

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ extern crate glam;
 use std::io::{Write,stdout};
 use std::fs::File;
 use clap::{Arg,App};
-use glam::{Mat4,Vec4};
+use glam::{Affine3A,Vec3};
 
 #[derive(Debug,Clone)]
 struct ArgParseError;
 
 fn collect_transforms(matches : &clap::ArgMatches, name : &str,
-		      f : impl Fn(&str) -> Result<Mat4,ArgParseError>)
-		      -> Result<Vec<(usize,Mat4)>,ArgParseError> {
+		      f : impl Fn(&str) -> Result<Affine3A,ArgParseError>)
+		      -> Result<Vec<(usize,Affine3A)>,ArgParseError> {
     let is = matches.indices_of(name);
     let vs = matches.values_of(name);
     let mut mats = Vec::new();
@@ -37,29 +37,46 @@ fn main() {
 	.arg(Arg::with_name("center")
 	     .help("Compute the center of the model's bounding box and move it to the origin.") )
 	.arg(Arg::with_name("output")
-	     .short("o")
+	     .short("o").long("output")
+	     .multiple(true)
+	     .number_of_values(1)
 	     .takes_value(true)
 	     .help("Name of the file to output the transformed file as, '--' for stdout") )
 	.arg(Arg::with_name("rx")
+	     .long("rx")
+	     .multiple(true)
+	     .number_of_values(1)
 	     .takes_value(true)
 	     .help("Rotate around the X axis by the specified number of degrees") )
 	.arg(Arg::with_name("ry")
+	     .long("ry")
+	     .multiple(true)
+	     .number_of_values(1)
 	     .takes_value(true)
 	     .help("Rotate around the Y axis by the specified number of degrees") )
 	.arg(Arg::with_name("rz")
+	     .long("rz")
+	     .multiple(true)
+	     .number_of_values(1)
 	     .takes_value(true)
 	     .help("Rotate around the Z axis by the specified number of degrees") )
 	.arg(Arg::with_name("translate")
-	     .short("t")
+	     .short("t").long("translate")
 	     .multiple(true)
 	     .number_of_values(1)
 	     .takes_value(true)
 	     .help("Translate by the given X,Y,Z triple") )
 	.arg(Arg::with_name("mx")
+	     .long("mx")
+	     .multiple(true)
 	     .help("Mirror the model across the YZ plane") )
 	.arg(Arg::with_name("my")
+	     .long("my")
+	     .multiple(true)
 	     .help("Mirror the model across the XZ plane") )
 	.arg(Arg::with_name("mz")
+	     .long("mz")
+	     .multiple(true)
 	     .help("Mirror the model across the XY plane") );
 
     let matches = app.get_matches();
@@ -68,13 +85,22 @@ fn main() {
 	None => Box::new(stdout()) as Box<dyn Write>,
 	Some(x) => Box::new(File::create(x).unwrap()) as Box<dyn Write>,
     };
-    let mut transformations : Vec<(usize,Mat4)> = Vec::new();
-    fn make_translation(arg : &str) -> Result<Mat4,ArgParseError> {
+    let mut transformations : Vec<(usize,Affine3A)> = Vec::new();
+    fn make_translation(arg : &str) -> Result<Affine3A,ArgParseError> {
 	let args : Vec<&str> = arg.split(",").collect();
 	if args.len() != 3 {
 	    return Err(ArgParseError);
 	}
-	Ok(Mat4::IDENTITY)
+	let mut coords : [f32; 3] = [0.0; 3];
+	for (i,v) in args.iter().enumerate() {
+	    coords[i] = match v.parse::<f32>() {
+		Ok(val) => val,
+		Err(_) => return Err(ArgParseError),
+	    }
+	}
+	println!("Parsed: {} {} {} ",coords[0],coords[1],coords[2]);
+	let v = Vec3::new(coords[0],coords[1],coords[2]);
+	Ok(Affine3A::from_translation(v))
     }
     collect_transforms(&matches, "translate", make_translation );
 		    
